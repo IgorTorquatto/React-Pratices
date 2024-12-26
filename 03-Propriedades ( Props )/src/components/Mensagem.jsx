9562d4e2f925ed50674ce375f6401997ce55967f
Igor <igor.torquato.20@gmail.com>
2024-12-26 14:49:35 -0300

    att: prop-types

--- 03-Propriedades ( Props )/src/components/Mensagem.jsx
+++ 03-Propriedades ( Props )/src/components/Mensagem.jsx
@@ -1,8 +1,14 @@
+import PropTypes from "prop-types"
+
 export function Mensagem({msg}){
     return(
         <p>{msg}</p>
     )
 }
+
+Mensagem.propTypes = {
+    msg: PropTypes.string
+} 
 
 Mensagem.defaultProps = {
     msg: 'Default message'
--- 03-Propriedades ( Props )/src/components/Title.jsx
+++ 03-Propriedades ( Props )/src/components/Title.jsx
@@ -1,7 +1,13 @@
+import PropTypes from "prop-types"
+
 export function Title(props){
     return(
         <h1>{props.titulo}</h1>
     )
+}
+
+Title.propTypes = {
+    titulo: PropTypes.string,
 }
 
 Title.defaultProps = {
